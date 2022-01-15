f3ee63c4c2685e846fe75b702234134677a2523d
Jiaxin Liu <jiaxinliu.victor@gmail.com>
2022-01-14 20:41:12 -0500

    a

--- server/routes/api/task.js
+++ server/routes/api/task.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid');
 const express = require('express');
-const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ const dbURL = "mongodb+srv://virtuous:312528@cluster0.g1fqn.mongodb.net/myFirstD
 
 async function loadTask() {
     try {
-        const client = await mongoose.connect(dbURL, {useNewUrlParser: true});
+        const client = await mongodb.MongoClient.connect(process.env.dbURL, {useNewUrlParser: true});
         collection = client.db('TaskDB').collection('tasks');
         console.log('DB CONNECTED SUCCESSFULLY');
         return collection;   
@@ -40,19 +40,20 @@ router.post('/', async (req, res) => {
         res.status(201).json("added successfully");
     }
 });
+
 //Delete task
 router.delete('/:id', async (req, res) => {
     const tasks = await loadTask();
-    await tasks.deleteOne({_id: mongoose.ObjectId(req.params.id)});
+    await tasks.deleteOne({_id: mongodb.ObjectId(req.params.id)});
     res.status(200).send('delete successfully');
 });
 
 //Update reminder of task
 router.put('/reminder/:id', async (req, res) => {
     const tasks = await loadTask();
-    const task = await tasks.findOne({_id: mongoose.ObjectId(req.params.id)});
+    const task = await tasks.findOne({_id: mongodb.ObjectId(req.params.id)});
     await tasks.updateOne(
-        {_id: mongoose.ObjectId(req.params.id)},
+        {_id: mongodb.ObjectId(req.params.id)},
         {
             $set: {
                 reminder: !task.reminder
